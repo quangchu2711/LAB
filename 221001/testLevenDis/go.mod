8cd32a43a5c23f767c89ba3a892f7672f1cf3c1a
Quang <quangcx2711@gmail.com>
2022-10-03 15:23:36 +0700

    Benchmarking levenshtien distance

---
+++ 221001/testLevenDis/go.mod
@@ -0,0 +1,10 @@
+module leven
+
+go 1.18
+
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/arbovm/levenshtein v0.0.0-20160628152529-48b4e1c0c4d0 // indirect
+	github.com/dgryski/trifles v0.0.0-20220729183022-231ecf6ed548 // indirect
+	github.com/ka-weihe/fast-levenshtein v0.0.0-20201227151214-4c99ee36a1ba // indirect
+)
---
+++ 221001/testLevenDis/leven_test.go
@@ -0,0 +1,52 @@
+package testLeven
+
+import (
+	"testing"
+
+	agnivade "github.com/agnivade/levenshtein"
+	arbovm "github.com/arbovm/levenshtein"
+	dgryski "github.com/dgryski/trifles/leven"
+	kaweihe "github.com/ka-weihe/fast-levenshtein"
+)
+
+var sink int
+func BenchmarkAll(b *testing.B) {
+	tests := []struct {
+		a, b string
+		name string
+	}{
+		// ASCII
+		{"levenshtein", "frankenstein", "ASCII"},
+		// Testing acutes and umlauts
+		{"resumé and café", "resumés and cafés", "French"},
+		{"Hafþór Júlíus Björnsson", "Hafþor Julius Bjornsson", "Nordic"},
+		// Only 2 characters are less in the 2nd string
+		{"།་གམ་འས་པ་་མ།", "།་གམའས་པ་་མ", "Tibetan"},
+	}
+	tmp := 0
+	for _, test := range tests {
+		b.Run(test.name, func(b *testing.B) {
+			b.Run("agniva", func(b *testing.B) {
+				for n := 0; n < b.N; n++ {
+					tmp = agnivade.ComputeDistance(test.a, test.b)
+				}
+			})
+			b.Run("arbovm", func(b *testing.B) {
+				for n := 0; n < b.N; n++ {
+					tmp = arbovm.Distance(test.a, test.b)
+				}
+			})
+			b.Run("kaweihe", func(b *testing.B) {
+				for n := 0; n < b.N; n++ {
+					tmp = kaweihe.Distance(test.a, test.b)
+				}
+			})
+			b.Run("dgryski", func(b *testing.B) {
+				for n := 0; n < b.N; n++ {
+					tmp = dgryski.Levenshtein([]rune(test.a), []rune(test.b))
+				}
+			})
+		})
+	}
+	sink = tmp
+}
